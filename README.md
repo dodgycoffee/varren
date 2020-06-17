4e65fa0c0f5c9edb6bfb40d918da628df6054373
Alex Brindley <alex@dodgyco.de>
2020-06-17 15:48:33 -0400

    First build

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-varren
+# Varren
+A minimal gulp 4 build starter
+
+## Installation
+Install is nice and simple. Open a brand new project, and just run:
+```
+npx dodgycoffee/varren
+```
+
+## Commands
+After install is complete, the following build commands will be available:
+
+- `gulp test` verifies gulp is working. If this fails you'll need to run `npm install --global gulp-cli`
+- `gulp build` runs the production build step (minification, sourcemaps, image optimisation, etc)
+- `gulp optimise` runs the image optimiser only (good for right after adding a bunch of new source images)
+- `gulp watch` runs the watcher. This will monitor scss/js files for changes and auto-build development on save. Excludes the image optimisation step
+
+> To add or modify commands, simple edit your gulpfile.js file
+
+## FAQ
+- **How do I do *specifically* so and so?**
+    - It's impossible I quit.
---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+const gulp          = require('gulp');
+const sass          = require('gulp-sass');
+const sassGlob      = require('gulp-sass-glob');
+const imagemin      = require('gulp-imagemin');
+const uglify        = require('gulp-uglify');
+const sourcemaps    = require('gulp-sourcemaps');
+
+const path = {
+    styles: {
+        src: 'resources/sass/styles.scss',
+        dest: 'web/assets/css',
+        watch: 'resources/sass/**/*.scss'
+    },
+    js: {
+        src: 'resources/js/**/*',
+        dest: 'web/assets/js',
+    },
+    images: {
+        src: 'resources/img/*',
+        dest: 'web/assets/img',
+    }
+};
+
+function test(cb) {
+    console.log('Gulp is functioning!');
+    cb();
+}
+
+function scss(cb) {
+    return gulp.src(path.styles.src)
+        .pipe(sourcemaps.init())
+        .pipe(sassGlob())
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.styles.dest));
+    cb();
+}
+
+function js(cb) {
+    return gulp.src(path.js.src)
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(gulp.dest(path.js.dest));
+    cb();
+}
+
+function optimiseImages() {
+    return gulp.src(path.images.src)
+        .pipe(imagemin())
+        .pipe(gulp.dest(path.images.dest));
+}
+
+function watchFiles() {
+    gulp.watch(path.styles.watch, scss);
+    gulp.watch(path.js.src, js);
+}
+
+exports.test        = test;
+exports.build       = gulp.series(scss, js, optimiseImages);
+exports.optimise    = optimiseImages;
+exports.watch       = watchFiles;
